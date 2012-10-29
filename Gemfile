d2b01b6e6fe9e15aba8cc1667007cd0e973c04db
Kaile Liang <liangkaile@gmail.com>
2012-10-28 23:53:13 -0600

    search success

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ end
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
@@ -36,4 +36,4 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'debugger'
-gem 'will_paginate', '~> 3.0'
+ gem 'will_paginate', '~> 3.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,24 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 module ApplicationHelper
-  def hidden_div_if(condition, attributes = {}, &block)
-    if condition
-      attributes["style"] = "display: none"
+    def hidden_div_if(condition, attributes = {}, &block)
+        if condition
+            attributes["style"] = "display: none"
+        end
+        content_tag("div", attributes, &block)
     end
-    content_tag("div", attributes, &block)
-  end
+
+    #def sortable(column, title = nil)
+    #    title ||= column.titleize
+    #    direction = column == params[:sort] && params[:direction]== "asc" ? "desc" : "asc"
+    #    link_to title, :sort => column, :direction => direction
+    #end
+    
+def sortable(column, title = nil)
+  title ||= column.titleize
+  css_class = column == sort_column ? "current #{sort_direction}" : nil
+  direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+  link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+end
+
 end
